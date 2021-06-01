8185eccee712f040f39f2a2687eb431a8ac89a2a
Robert Saramet <robertsaramet@pm.me>
2021-06-02 01:41:30 +0300

    Update and rename arduino/Robot/Robot.cpp to arduino/library/Robot.cpp

--- arduino/library/Robot.cpp
+++ arduino/library/Robot.cpp
@@ -6,6 +6,12 @@ Robot::Robot() {
     sonarL = Ultrasonic (L_TRIG, L_ECHO);
     sonarR = Ultrasonic (R_TRIG, R_ECHO);
     sonarF = Ultrasonic (F_TRIG, F_ECHO);
+
+    if (comms) {
+        // This connection is used for the joystick
+        ESP_PORT.begin(4800);
+        myTransfer.begin(ESP_PORT);
+    }
 }
 
 Robot::detectObstacle() {
@@ -98,6 +104,99 @@ Robot::followLine() {
 
             robot.setSpeedA(m_motorLeft);
             robot.setSpeedB(m_motorRight);
+        }
+    }
+}
+
+Robot::getJoystick() {
+    struct STRUCT {
+    int16_t pos_x1;
+    int16_t pos_y1;
+    bool sw1;
+    int16_t pos_x2;
+    int16_t pos_y2;
+    bool sw2;
+    } payload;
+
+    int mapX1, mapY1;
+    bool btn1 = 0;
+
+    int mapX2, mapY2;
+    bool btn2 = 0;
+
+    if (myTransfer.available()) {
+        // Load packet from ESP8266
+        myTransfer.rxObj(payload);
+
+        // Values for motors
+        mapX1 = payload.pos_x1;
+        mapY1 = payload.pos_y1;
+        bool btnOld1 = btn1;
+        btn1 = payload.sw1;
+
+        // Values for servo
+        mapX2 = payload.pos_x2;
+        mapY2 = payload.pos_y2;
+        bool btnOld2 = btn2;
+        btn2 = payload.sw2;
+
+        // Check m_brake button toggle
+        if (btn1 != btnOld1) {
+            if (btn1) {
+                m_brake = !m_brake;
+            }
+        }
+
+        if (m_brake) { // Press joystick button to stop robot
+            robot.stop();
+        }
+        else {
+            // Set robot speed & direction
+            if (mapY1 >= 0) {
+                robot.forward();
+            }
+            else {
+                robot.backward();
+                mapY1 *= -1;
+            }
+            if (mapX1 >= 0) {
+                robot.setSpeedB(mapY1);
+                robot.setSpeedA(mapY1 - mapX1);
+            }
+            else {
+                mapX1 *= -1;
+                robot.setSpeedB(mapY1 - mapX1);
+                robot.setSpeedA(mapY1);
+            }
+        }
+
+        // Change servo position
+        if (mapX2 > 100 || mapX2 < -100) {
+            if (mapX2 < 0) {
+                if (m_pos >= 2)
+                {
+                    m_pos -= 2;
+                }
+            }
+            else {
+                if (m_pos <= 168) {
+                    m_pos += 2;
+                }
+            }
+            servo.write(m_pos);
+        }
+
+        // Check laser button toggle
+        if (btn2 != btnOld2) {
+            if (btn2) {
+                m_pwr = !m_pwr;
+            }
+            if (m_pwr) {
+                digitalWrite(laser, HIGH);
+            }
+            else {
+                digitalWrite(laser, LOW);
+            }
         }
     }
 }
