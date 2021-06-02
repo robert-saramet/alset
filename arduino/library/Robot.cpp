e491e482a069f4e639f4b7fccf52e8f576955bf4
Robert Saramet <robertsaramet@pm.me>
2021-06-02 21:56:49 +0300

    Update Robot.cpp

--- arduino/library/Robot.cpp
+++ arduino/library/Robot.cpp
@@ -2,42 +2,48 @@
 #include <Robot.h>
 
 Robot::Robot() { 
-    motors = L298NX2 (MOTOR_A_EN, MOTOR_A1, MOTOR_A2, MOTOR_B_EN, MOTOR_B1, MOTOR_B2);
-    sonarL = Ultrasonic (L_TRIG, L_ECHO);
-    sonarR = Ultrasonic (R_TRIG, R_ECHO);
-    sonarF = Ultrasonic (F_TRIG, F_ECHO);
+    m_motors = L298NX2 (MOTOR_A_EN, MOTOR_A1, MOTOR_A2, MOTOR_B_EN, MOTOR_B1, MOTOR_B2);
+    m_sonarL = Ultrasonic (L_TRIG, L_ECHO);
+    m_sonarR = Ultrasonic (R_TRIG, R_ECHO);
+    m_sonarF = Ultrasonic (F_TRIG, F_ECHO);
+    m_myTransfer = SerialTransfer;
+    m_servo = Servo;
 
     if (comms) {
         // This connection is used for the joystick
         ESP_PORT.begin(4800);
-        myTransfer.begin(ESP_PORT);
+        m_myTransfer.begin(ESP_PORT);
     }
+
+    filter = Adafruit_Madgwick;
+    cal = Adafruit_Sensor_Calibration_EEPROM;
+    m_timestamp = millis();
 }
 
 Robot::detectObstacle() {
-    if(sonarF.read() < 20){
-            motors.backward();
+    if(m_sonarF.read() < 20){
+            m_motors.backward();
             delay(80);
             return 0;
         }
-        if (sonarL.read() < 30 || sonarR.read() < 30) {
+        if (m_sonarL.read() < 30 || m_sonarR.read() < 30) {
             leftTrack = 1;
             robot.setSpeed(255);
-            if (sonarL.read() < sonarR.read()) {
-                while (sonarL.read() < 35) {
-                    motors.forwardA();
-                    motors.backwardB();
+            if (m_sonarL.read() < m_sonarR.read()) {
+                while (m_sonarL.read() < 35) {
+                    m_motors.forwardA();
+                    m_motors.backwardB();
                 }
             }
-            else if (sonarL.read() > sonarR.read()) {
-                while (sonarR.read() < 35) {
-                    motors.forwardB();
-                    motors.backwardA();
+            else if (m_sonarL.read() > m_sonarR.read()) {
+                while (m_sonarR.read() < 35) {
+                    m_motors.forwardB();
+                    m_motors.backwardA();
                 }
             }
             else { // If both sensors return same value, to avoid getting stuck
-                motors.backwardFor(300);
-                motors.reset();
+                m_motors.backwardFor(300);
+                m_motors.reset();
             }
             return 0;
         }
@@ -124,17 +130,17 @@ Robot::getJoystick() {
     int mapX2, mapY2;
     bool btn2 = 0;
 
-    if (myTransfer.available()) {
+    if (m_myTransfer.available()) {
         // Load packet from ESP8266
-        myTransfer.rxObj(payload);
+        m_myTransfer.rxObj(payload);
 
-        // Values for motors
+        // Values for m_motors
         mapX1 = payload.pos_x1;
         mapY1 = payload.pos_y1;
         bool btnOld1 = btn1;
         btn1 = payload.sw1;
 
-        // Values for servo
+        // Values for m_servo
         mapX2 = payload.pos_x2;
         mapY2 = payload.pos_y2;
         bool btnOld2 = btn2;
@@ -170,7 +176,7 @@ Robot::getJoystick() {
             }
         }
 
-        // Change servo position
+        // Change m_servo position
         if (mapX2 > 100 || mapX2 < -100) {
             if (mapX2 < 0) {
                 if (m_pos >= 2)
@@ -183,7 +189,7 @@ Robot::getJoystick() {
                     m_pos += 2;
                 }
             }
-            servo.write(m_pos);
+            m_servo.write(m_pos);
         }
 
         // Check laser button toggle
@@ -198,5 +204,60 @@ Robot::getJoystick() {
                 digitalWrite(laser, LOW);
             }
         }
+    }
+}
+
+Robot::getAngle(char angle = 'z') {
+    float gx, gy, gz;
+    static uint8_t counter = 0;
+
+    if ((millis() - m_timestamp) < (1000 / FILTER_UPDATE_RATE_HZ)) {
+        return;
+    }
+
+    m_timestamp = millis();
+
+    // Read the motion sensors
+    sensors_event_t accel, gyro, mag;
+    accelerometer->getEvent(&accel);
+    gyroscope->getEvent(&gyro);
+    magnetometer->getEvent(&mag);
+
+    // Read calibration data from EEPROM
+    cal.calibrate(mag);
+    cal.calibrate(accel);
+    cal.calibrate(gyro);
+
+    // Gyroscope needs to be converted from Rad/s to Degree/s
+    // the rest are not unit-important
+    gx = gyro.gyro.x * SENSORS_RADS_TO_DPS;
+    gy = gyro.gyro.y * SENSORS_RADS_TO_DPS;
+    gz = gyro.gyro.z * SENSORS_RADS_TO_DPS;
+
+    // Update the SensorFusion filter
+    filter.update(gx, gy, gz,
+                  accel.acceleration.x, accel.acceleration.y, accel.acceleration.z,
+                  mag.magnetic.x, mag.magnetic.y, mag.magnetic.z);
+
+    // Only returns the calculated output once in a while
+    if (counter++ <= IMU_BUFFER) {
+        return;
+    }
+    counter = 0;
+
+    roll = filter.getRoll();
+    pitch = filter.getPitch();
+    heading = filter.getYaw();
+
+    switch (angle) {
+        case 'X':
+        case 'x':
+            return roll;
+        case 'Y':
+        case 'y':
+            return pitch;
+        case 'Z':
+        case 'z':
+            return heading;
     }
 }
