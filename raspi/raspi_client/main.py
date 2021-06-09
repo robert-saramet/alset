5c50e82b00b30d14f08ad782243456e2a270b264
Robert Saramet <robertsaramet@pm.me>
2021-06-09 18:34:49 +0300

    Update main.py

--- raspi/raspi_client/main.py
+++ raspi/raspi_client/main.py
@@ -1,53 +1,50 @@
-# if a stop sign is detected, wait for it to be in sight for 3 seconds,
-# then continue moving until it isn't detected anymore
+# if a stop sign is detected and in range, wait for it to be in sight
+# for 3 seconds, then continue moving until it isn't detected anymore
 
 import signs
-import cv2
+from signs import cv2
+import gps
 import numpy as np
-import sys
+import serial
 import time
+import _thread as thread
+import networking as net
 from gpiozero import LED
-
-# not used for beta version
-import rfid
-import networking as net # server comms
-
-move = LED(26) # gpio comms
 stop_handler = signs.StopSignHandler()
 last_tick_halt = 0
+
+# 26 = comms pin
+# on() -> sends 1
+# off() -> sends 0
+comms_pin = LED(26)
+comms_pin.on()
+
+def SendMsg_Stop():
+	print("STOP RIGHT THERE U MF")
+	comms_pin.off()
+def SendMsg_Forward():
+	print("MOVE UR FCKING ASS U PIECE OF SHIT")
+	comms_pin.on()
+	
 
 cap = cv2.VideoCapture(0)
 delta_time_start = 0.0
 delta_time_end = 0.0
-
-move.on()
-
 while True:
-	try:
-		delta_time = delta_time_end - delta_time_start
-		delta_time_start = time.perf_counter() # delta time in seconds
+	delta_time = delta_time_end - delta_time_start
+	delta_time_start = time.perf_counter() # Time in seconds
 
-		# Camera input
-		frame = cap.read()[1]
+	# Camera input
+	frame = cap.read()[1]
 
-		# if display connected show opencv feed
-		'''
-		cv2.imshow('frame', frame)
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-		'''
+	(x, y, w, h) = stop_handler.Update(frame, delta_time, SendMsg_Stop, SendMsg_Forward)
 
-		stop_handler.Update(frame, delta_time)
+	cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-		if stop_handler.halt:
-			move.off()
-			print('HALT')
-		else:
-			print('FORWARD')
-			move.on()
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		comms_pin.off()
+		break
 
-		delta_time_end = time.perf_counter()
-	except KeyboardInterrupt:
-		print("Closing now")
-		move.off()
-		sys.exit()
+	cv2.imshow('frame', frame)
+
+	delta_time_end = time.perf_counter()
