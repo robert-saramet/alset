4a7a2d59bc58b86ef4470ab0afd5feaad344c453
Robert Saramet <robertsaramet@pm.me>
2021-06-09 19:13:16 +0300

    Update networking.py

--- raspi/raspi_client/networking.py
+++ raspi/raspi_client/networking.py
@@ -3,25 +3,25 @@ import socket
 import json
 import sys
 
-def ConnectToServer(pos):
-	id = sys.argv[1]
-	server_host = 'localhost'
-	server_port = '8080'
-	url = 'http://' + server_host + ':' + server_port + '/api'
-	ip = socket.gethostbyname(socket.gethostname())
-	port = 8081
-	data = {
-		"name": "rasppi",
-		"id": id, # factory id (?)
-		"ip": ip,
-		"x": pos[0],
-		"y": pos[1]
-	}
-	requests.post(url=url, json=data)
-	
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.bind((ip, port))
-	sock.listen()
-	(conn, addr) = sock.accept()
-	str = conn.recv(1024)
-	return (json.loads(str)['command']['x'] - 1, json.loads(str)['command']['y'] - 1)
+def ConnectToServer(pos, server_addr):
+        id = sys.argv[1]
+        server_host = server_addr[0]
+        server_port = server_addr[1]
+        url = 'http://' + server_host + ':' + server_port + '/api'
+        ip = '192.168.0.129'
+        port = 8081
+        data = {
+                "name": "rasppi",
+                "id": id, # factory id (?)
+                "ip": ip,
+                "x": pos[0],
+                "y": pos[1]
+        }
+        requests.post(url=url, json=data)
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((ip, port))
+        sock.listen()
+        (conn, addr) = sock.accept()
+        str = conn.recv(1024)
+        return (json.loads(str)['command']['x'] - 1, json.loads(str)['command']['y'] - 1)
