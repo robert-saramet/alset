ca750bfc3ccc53e6c915c655b227e520a07c34ed
Robert Saramet <robertsaramet@pm.me>
2021-07-29 15:00:32 +0300

    Update main.cpp

--- arduino/micro_328pb/main.cpp
+++ arduino/micro_328pb/main.cpp
@@ -24,8 +24,14 @@ struct __attribute__((__packed__)) MixedStruct {
 #define GPS_BAUD 9600
 
 TinyGPSPlus gps;
+TinyGPSCustom steerDirection(gps, "GPRMB", 3);
+
 LiquidCrystal_I2C lcd(0x27,20,4);
+
 I2CTransfer myTransfer;
+
+unsigned long lastUpdate = millis();
+short count = 0;
 
 void recvData() {
     uint16_t recSize = 0;
@@ -66,6 +72,7 @@ void readGPS() {
     if (gps.location.isUpdated()) {
         mixedData.lat = gps.location.lat();
         mixedData.lng = gps.location.lng();
+        mixedData.dir = steerDirection.value();
     }
     if (gps.speed.isUpdated()) {
         mixedData.speed = gps.speed.mps();
@@ -79,15 +86,46 @@ void sendData() {
 }
 
 void updateScreen() {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(F("Ultrasonic"));
-    lcd.setCursor(0, 1);
-    lcd.print(F("Left: "));
-    lcd.print(mixedData.distanceL);
-    lcd.setCursor(0, 2);
-    lcd.print(F("Right: "));
-    lcd.print(mixedData.distanceR);
+    if (millis() - lastUpdate >= 1200) {
+        lcd.clear();
+        smartDelay(100);
+        if (count == 0) {
+            lcd.setCursor(0, 0);
+            lcd.print(F("Ultrasonic"));
+            lcd.setCursor(0, 1);
+            lcd.print(F("Left: "));
+            lcd.print(mixedData.distanceL);
+            lcd.setCursor(0, 2);
+            lcd.print(F("Right: "));
+            lcd.print(mixedData.distanceR);
+            count++;
+        }
+        else if (count == 1) {
+            lcd.setCursor(0, 0);
+            lcd.print(F("GPS"));
+            lcd.setCursor(0, 1);
+            lcd.print(F("Latitude: "));
+            lcd.setCursor(10, 1);
+            lcd.print(mixedData.lat);
+            lcd.setCursor(0, 2);
+            lcd.print(F("Longitude: "));
+            lcd.setCursor(10, 2);
+            lcd.print(mixedData.lng);
+            lcd.setCursor(0, 3);
+            if (mixedData.dir == 'L') {
+                lcd.print(F("Steer left"));
+            }
+            else if (mixedData.dir == 'R') {
+                lcd.print(F("Steer right"));
+            }
+            count++;
+        }
+        else {
+            count = 0;
+            updateScreen();
+        }
+        lastUpdate = millis();
+    }
 }
 
 void loop() {
@@ -97,6 +135,5 @@ void loop() {
     readGPS();
     smartDelay(50);
     sendData();
-    smartDelay(50);
     updateScreen();
 }
