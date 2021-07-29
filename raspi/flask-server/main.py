641b71042c9767c2ce3735b6ea8532e1c798fb29
BoboMac <30493004+BoboMac@users.noreply.github.com>
2021-07-29 18:02:07 +0300

    Add files via upload

--- raspi/flask-server/main.py
+++ raspi/flask-server/main.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, json, render_template, request, jsonify
+from herepy.models import RoutingResponse
 from herepy.routing_api import RoutingApi
 import requests
 import urllib.parse
@@ -16,6 +17,7 @@ class GPS:
 		return self.api.car_route(self.current_location, destination)
 
 app = Flask(__name__)
+
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -40,10 +42,19 @@ def test_func():
 
 	gps.current_location = (request.get_json()['current_lat'], request.get_json()['current_long'])
 
-	print(gps.get_route((location.latitude, location.longitude)))
+	route = gps.get_route((location.latitude, location.longitude))
+	
+	route_coords = []
+	for value in route.as_dict()['response']['route'][0]['leg'][0]['maneuver']:
+		route_coords.append((value['position']['latitude'], value['position']['longitude']))
+
+	end_coords_json = route.as_dict()['response']['route'][0]['leg'][0]['end']['mappedPosition']
+	end_coords = (end_coords_json['latitude'], end_coords_json['latitude'])
+
+	print("\n\n", route_coords, "\n\n")
 
 	return jsonify({ 'status': 1 })
 
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.run()
 
