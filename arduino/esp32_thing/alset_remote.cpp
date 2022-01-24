f0468abaa84b05eb3f79b540c189556ec8bef3fb
Robert Saramet <robertsaramet@pm.me>
2022-01-24 17:01:06 +0200

    Create alset_remote.cpp

---
+++ arduino/esp32_thing/alset_remote.cpp
@@ -0,0 +1,66 @@
+#include "SerialTransfer.h"
+#include <esp_now.h>
+#include <WiFi.h>
+
+#define DEBUG
+
+struct RemoteData {
+    int relSpeed, pos;
+    bool turbo, autoPilot;
+} remDat = {0, 90, 0, 0};
+
+struct __attribute__((__packed__)) MotorStruct {
+    int8_t speed;
+    uint8_t pos;
+    bool turbo;
+} motors;
+
+long lastRecv = millis();
+
+SerialTransfer serialTransfer;
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+    memcpy(&remDat, incomingData, sizeof(remDat));
+    #ifdef DEBUG
+        Serial.print(F("Bytes received: "));
+        Serial.println(len);
+        Serial.print(F("Time since last input: "));
+        Serial.print(millis() - lastRecv);
+        Serial.println(F("ms"));
+    #endif
+    lastRecv = millis();
+}
+
+void sendSpeed() {
+    uint16_t sendSize = 0;
+    if (millis() - lastRecv > 200)
+        remDat = {0, 90, 0, 0};
+    motors.speed = remDat.relSpeed;
+    motors.pos = remDat.pos;
+    motors.turbo = remDat.turbo;
+    sendSize = serialTransfer.txObj(motors, sendSize);
+    serialTransfer.sendData(sendSize);
+}
+
+void initComms() {
+    Serial.begin(115200);
+    Serial2.begin(115200);
+    serialTransfer.begin(Serial2);
+    WiFi.mode(WIFI_STA);
+    if (esp_now_init() != ESP_OK) {
+        #ifdef DEBUG
+            Serial.println(F("Error initializing ESP-NOW"));
+        #endif
+    return;
+  }
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void setup() {
+    initComms();
+    delay(200);
+}
+
+void loop() {
+    sendSpeed();
+}
